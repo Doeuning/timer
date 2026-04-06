4b1d98bb2d01444b6a4a35aa8881a527312f008d
Doeuning <doeuning@gmail.com>
2026-04-06 14:09:06 +0900

    fix: 1분 노출 오류 수정 feat: main.js에서 시간설정 하도록 수정

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const { autoUpdater } = require("electron-updater");
 
 let mainWindow;
 let isShown = false;
-let hour = 18,
-  min = 0,
+let hour = 14,
+  min = 7,
   sec = 0,
   ms = 0;
 
@@ -63,32 +63,59 @@ function hideWindow() {
   }
 }
 
+function getState(diff) {
+  // 10분 10초 전
+  if (diff <= 610000 && diff > 600000) {
+    return { diff: diff, type: "tenMin", message: "퇴근 10분 전입니다 😎" };
+  }
+
+  // 30초 전 ~ 퇴근 전까지
+  if (diff <= 30000 && diff > 0) {
+    return { diff: diff, type: "thirtySec", message: "퇴근까지 남은 시간 🏃" };
+  }
+
+  // 퇴근시간 후
+  if (diff < 0) {
+    return {
+      diff: Math.floor(diff / 1000),
+      type: "overtime",
+      message: "초과근무 중 🥱",
+    };
+  }
+
+  // 그 외
+  return { diff: null, type: "hidden", message: "" };
+}
+
+function sendState() {
+  const now = new Date();
+
+  const target = new Date();
+  target.setHours(hour, min, sec, ms);
+
+  let diff = target - now;
+
+  const totalSec = Math.floor(diff / 1000);
+  const state = getState(diff);
+
+  mainWindow.webContents.send("update-state", {
+    ...state,
+    totalSec,
+  });
+
+  if (state.type === "hidden") {
+    hideWindow();
+  } else if (state.type === "tenMin" && !isShown) {
+    showWindow();
+    setTimeout(hideWindow, 10000);
+  } else {
+    showWindow();
+  }
+}
+
 function checkTimeAndShow() {
   setInterval(() => {
-    const now = new Date();
-
-    const target = new Date();
-    target.setHours(hour, min, sec, ms); // 퇴근 시간
-
-    const diff = target - now;
-    // 1초 1000
-    // 10초 10000
-    // 1분 60 * 1000
-    // 10분 600 * 1000
-    // 1시간 3600 * 1000
-
-    // 10분 10초 전
-    if (diff <= 611000 && diff > 609000 && !isShown) {
-      showWindow();
-      setTimeout(() => {
-        hideWindow();
-      }, 10000);
-    }
-
-    // 30초 전 ~ 퇴근 전까지
-    if (diff <= 30000 && diff > 0 && !isShown) {
-      showWindow();
-    }
+    sendState();
   }, 1000);
 }
 
--- renderer.js
+++ renderer.js
@@ -1,109 +1,65 @@
 const { ipcRenderer } = require("electron");
 const fitty = require("fitty");
 
-let targetTime = { hour: 18, min: 0, sec: 0, ms: 0 };
-
-ipcRenderer.on("set-time", (event, data) => {
-  targetTime = data;
-});
-
-// 현재시각 표시 타이머
-// function startTimer(el) {
-//   setInterval(() => {
-//     if (!el) return;
-//     const curr = new Date();
-//     const utc = curr.getTime() + curr.getTimezoneOffset() * 60 * 1000;
-//     const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
-//     const kr_curr = new Date(utc + KR_TIME_DIFF);
-//     const timeStr = kr_curr.toLocaleTimeString("en-GB", { hour12: true });
-
-//     el.textContent = timeStr;
-//   }, 1000);
-// }
-
-function startTimer() {
+ipcRenderer.on("update-state", (event, data) => {
+  const box = document.getElementById("box");
   const timer = document.getElementById("timer");
   const timerHour = document.getElementById("timer-hour");
   const timerMin = document.getElementById("timer-min");
   const timerSec = document.getElementById("timer-sec");
   const title = document.getElementById("title");
+  const totalSec =
+    data.type === "overtime" ? Math.abs(data.totalSec) : data.totalSec;
 
-  if (!timer) return;
-  setInterval(() => {
-    const now = new Date();
+  // 타이머 초기화
+  timerHour.style.display = "none";
+  timerMin.style.display = "none";
+  timerSec.style.display = "none";
 
-    const target = new Date();
+  // 클래스 초기화
+  timer.classList.remove("warning", "error");
 
-    target.setHours(
-      targetTime.hour,
-      targetTime.min,
-      targetTime.sec,
-      targetTime.ms,
-    );
+  // 메시지 업데이트
+  title.textContent = data.message;
 
-    let diff = target - now;
-    const totalSec = Math.floor(diff / 1000);
-    let hour = `${Math.floor(totalSec / 3600)}`;
-    let min = `${Math.floor((totalSec % 3600) / 60)}`;
-    let sec = `${totalSec % 60}`;
-    title.textContent = "퇴근까지 남은 시간";
-    // let ms = `${diff % 1000}ms`;
+  // 퇴근 10분 전
+  if (data.type === "tenMin") {
+    return;
+  }
 
-    if (hour > 0) {
-      timerHour.querySelector(".unit-number").textContent = hour;
-      timerHour.style.display = "inline";
-    }
+  // 퇴근 30초 전
+  if (data.type === "thirtySec") {
+    box.classList.add("warning");
+  }
 
-    if (min > 0) {
-      timerMin.querySelector(".unit-number").textContent = min;
-      timerMin.style.display = "inline";
-    }
+  // 초과근무
+  if (data.type === "overtime") {
+    box.classList.add("overtime");
+  }
 
-    if (sec >= 0) {
-      timerSec.querySelector(".unit-number").textContent = sec;
-      timerSec.style.display = "inline";
-    }
+  // 시간 계산
+  let hour = `${Math.floor(totalSec / 3600)}`;
+  let min = `${Math.floor((totalSec % 3600) / 60)}`;
+  let sec = `${totalSec % 60}`;
 
-    if (totalSec >= 0 && totalSec <= 10) {
-      timer.classList.add("warning");
-      return;
-    } else if (totalSec < 0) {
-      // overtime
-      const overtime = Math.abs(diff);
-      const h = Math.floor(overtime / (1000 * 60 * 60));
-      const m = Math.floor((overtime / (1000 * 60)) % 60);
-      const s = Math.floor((overtime / 1000) % 60);
-      timer.classList.remove("warning");
-      timer.classList.add("error");
-      title.textContent = "초과근무 시간";
+  // 시간 표시
+  if (hour > 0) {
+    timerHour.querySelector(".unit-number").textContent = hour;
+    timerHour.style.display = "inline";
+  }
 
-      if (h > 0) {
-        timerHour.querySelector(".unit-number").textContent = h;
-        timerHour.style.display = "inline";
-      }
-      if (m > 0) {
-        timerMin.querySelector(".unit-number").textContent = m;
-        timerMin.style.display = "inline";
-      }
-      if (s >= 0) {
-        timerSec.querySelector(".unit-number").textContent = s;
-        timerSec.style.display = "inline";
-      }
-      return;
-    }
+  if (min > 0 || hour > 0) {
+    timerMin.querySelector(".unit-number").textContent = min;
+    timerMin.style.display = "inline";
+  }
 
-    // 현재시각 표시 타이머
-    // const h = Math.floor(diff / (1000 * 60 * 60));
-    // const m = Math.floor((diff / (1000 * 60)) % 60);
-    // const s = Math.floor((diff / 1000) % 60);
-    // el.textContent = `${String(h).padStart(2, "0")}:${String(m).padStart(2, "0")}:${String(s).padStart(2, "0")}`;
-  }, 50);
-}
+  timerSec.querySelector(".unit-number").textContent = sec;
+  timerSec.style.display = "inline";
+});
 
 window.addEventListener("load", function () {
   // const btnClose = document.getElementById("btnClose");
 
-  startTimer();
   fitty("#timer");
 
   // btnClose.addEventListener("click", () => {
